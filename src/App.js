e3e2fa38290dd224c096081f4ee52d6db3abbb09
Dan0227 <jhojan0227@gmail.com>
2023-09-07 08:07:51 -0500

    Header, dropdown

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Encabezado from './Components/Encabezado';
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <Encabezado/>
+    </div>
+  );
+}
+
+export default App
